a19cf018ce9b359df7adcd98444e3f8db50b548b
Prem Ganeshkumar <ganeprem@gmail.com>
2016-06-03 19:46:54 -0400

    Define constants

--- run.py
+++ run.py
@@ -5,3 +5,21 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+# Our name
+BOT_NAME = '@updogbot'
+
+TWITTER_LIMIT = 140
+
+parser = SafeConfigParser()
+parser.read('secrets.cfg')
+
+# Twitter credentials
+CONSUMER_KEY = parser.get('Twitter', 'CONSUMER_KEY')
+CONSUMER_SECRET = parser.get('Twitter', 'CONSUMER_SECRET')
+ACCESS_KEY = parser.get('Twitter', 'ACCESS_KEY')
+ACCESS_SECRET = parser.get('Twitter', 'ACCESS_SECRET')
+
+if __name__ == '__main__':
+	listener = TweetListener()
+	stream = Stream(auth, listener)	
+	stream.filter(track=[BOT_NAME])
