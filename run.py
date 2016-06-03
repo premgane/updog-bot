6c216a8d5eb886d18251973371c490453f4cd23a
Prem Ganeshkumar <ganeprem@gmail.com>
2016-06-03 19:48:24 -0400

    Add classes. Set up tweepy api object

--- run.py
+++ run.py
@@ -19,6 +19,35 @@ CONSUMER_SECRET = parser.get('Twitter', 'CONSUMER_SECRET')
 ACCESS_KEY = parser.get('Twitter', 'ACCESS_KEY')
 ACCESS_SECRET = parser.get('Twitter', 'ACCESS_SECRET')
 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+# Tweet class with all the information we need for this program (Hashtags and the actual tweet text)
+class Tweet:
+	text = str()
+	hashtags = []
+	urls = []
+	full = {}
+
+	def __init__(self, json):
+		self.text = json.get('text', '')
+		self.hashtags = json.get('entities', {}).get('hashtags', [])
+		self.urls = json.get('entities', {}).get('urls', [])
+		self.full = json
+
+# Basic listener which parses the json, creates a tweet, and sends it to parseTweet
+class TweetListener(StreamListener):
+	def on_data(self, data):
+		jsonData = json.loads(data)
+		tweet = Tweet(jsonData)
+		parseTweet(tweet)
+		return True
+
+	def on_error(self, status):
+		print status
+
 if __name__ == '__main__':
 	listener = TweetListener()
 	stream = Stream(auth, listener)	
